eae3e8cd23e4b32623b9f0d1b6f6a42ef18fcbb8
Joseph <josephiusx@gmail.com>
2021-04-20 10:03:40 -0700

    L.10 Importing your own file

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Node-course
+
+sec3 L.10
+    If I require utils.js in my app.js file, both files will run when I run app.js
+
+    even though I require utils.js its its variables are not avalible to app.js and it goes both ways. they are locally scoped
+
+    this is why we use module.exports
--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,17 @@
 
-const fs = require('fs'); // loading the node file system lib
+const getNotes = require('./notes.js');
+console.log(getNotes());
 
-// fs.writeFileSync('notes.txt', 'My name is Joseph.') // creates a file with the node js fs(file System) built in module
 
-fs.appendFileSync('notes.txt', 'data appended');
+
+
+
+
+
+
+
+
+
+
+
+
