9e08a700376e91deb732dd94e581d00005a5a467
Joseph <josephiusx@gmail.com>
2021-04-20 12:58:08 -0700

    15. Getting Input from Users

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,13 +1,12 @@
 const chalk = require('chalk');
 const getNotes = require('./notes.js');
 
-const msg = getNotes()
-console.log(msg);
+const command = process.argv[2] // naming the third command line argument
 
-console.log(chalk.bold.red.inverse('Hello0000 world!'));
+console.log(process.argv);  // shows me the command line arguments
 
-
-
-
-
-
+if (command === 'add') { // if the command is : node app.js add
+    console.log('Adding note!')
+} else if (command === 'remove') { // else if : node app.js remove
+    console.log('Removing note!')
+}
