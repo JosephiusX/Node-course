6ee307cc4705497f8cf94eb619a8964bb9208979
Joseph <josephiusx@gmail.com>
2021-04-20 18:10:44 -0700

    17. Argument Parsing with Yargs: Part 2

--- notes-app/app.js
+++ notes-app/app.js
@@ -10,8 +10,21 @@ yargs.version('1.1.0')
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function () {
-        console.log('Adding a new note!')
+    builder:  {
+       title: {
+           describe: 'Note title',
+           demandOption: true, // meaning it has to be provided in order for the command to work
+           type: 'string'
+       },
+       body: {
+           describe: 'Note body', 
+           demandOption: true,
+           type: 'string'
+       }
+    },
+    handler: function (argv) {
+        console.log('Title:' + argv.title)
+        console.log('Body: ' + argv.body)
     }
 })
 
@@ -29,7 +42,7 @@ yargs.command({
     command: 'list',
     describe: 'list a note',
     handler: function () {
-        console.log('listing note')
+        console.log('listing all notes')
     }
 })
 
@@ -43,4 +56,6 @@ yargs.command({
 })
 
 
-console.log(yargs.argv); // yargs version of arg v
+// console.log(yargs.argv); // yargs version of arg v
+
+yargs.parse() // runs yargs as well as 
