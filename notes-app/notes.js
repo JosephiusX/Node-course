e8f0a0e2802b556f5b6ae2bbc998029b7813b46a
Joseph <josephiusx@gmail.com>
2021-04-30 15:18:40 -0700

    adding doccumentation

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -4,22 +4,23 @@ const getNotes = () => {
     return 'Your notes...'
 }
 
-const addNote = (title,body) => {
+const addNote = (title,body) => { // function responsible for adding a note
     const notes = loadNotes()
-    const duplicateNotes = notes.filter(function (note) {
-        return note.title === title
+    const duplicateNotes = notes.filter(function (note) { // this function is going to run for every item in notes array
+        return note.title === title // using equality operator to check if note.title equals title, returns a boolean
     })
 
-    if(duplicateNotes.length === 0) {
-        notes.push({
+    if(duplicateNotes.length === 0) { // if we didnt find any duplicates
+        notes.push({ // we can safely create the note
             title: title,
             body: body
         })
+
         console.log(notes);
     
         saveNotes(notes)
         console.log('New note added!')
-    } else {
+    } else { // if there are duplicates
         console.log('Note title taken!')
     }
 
@@ -32,18 +33,18 @@ const saveNotes = (notes) => {
 }
 
 const loadNotes = () => {
-    try {
-        const dataBuffer = fs.readFileSync('notes.json')
-        const dataJSON = dataBuffer.toString()
-        return JSON.parse(dataJSON)
-    } catch (e) {
-        return []
+    try { // if either of thease lines throws an error.. 
+        const dataBuffer = fs.readFileSync('notes.json') // file we will be reading and writing to
+        const dataJSON = dataBuffer.toString() // convert dataBuffer into a string
+        return JSON.parse(dataJSON) // parse dataJSON
+    } catch (e) { // the code will stop and run catch instead
+        return [] // return empty array 
     }
 
     
 }
 
-module.exports = {
-    getNotes: getNotes,
+module.exports = { // set this to an object containing multiple functions instead of one
+    getNotes: getNotes, // property getNotes
     addNote: addNote
 }
