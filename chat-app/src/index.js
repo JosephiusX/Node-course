3e4496cab840ebb23a74ebc0ca85ba52559782ff
Joseph <josephiusx@gmail.com>
2021-08-09 09:54:25 -0700

    157. Brodcasting Events

---
+++ chat-app/src/index.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+
+const app = express()
+const server = http.createServer(app) // creating a server outside of express for use with socket.io
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+io.on('connection', (socket) => {
+	console.log('New websocket connection')
+
+	socket.emit('message', 'welcome') // emit to a particular connection
+	socket.broadcast.emit('message', 'A new user has joined!') // emit to everybody but that particular connection
+
+	socket.on('sendMessage', (message) => {
+		io.emit('message', message) // io.emmit to sent to everybody
+	})
+
+	// code runs whenever client disconnects
+	socket.on('disconnect', () => {
+		io.emit('message', 'A user has left!') // sends message to clients still connected
+	})
+})
+
+server.listen(port, () => {
+	console.log(`Server is up on port ${port}`)
+})
